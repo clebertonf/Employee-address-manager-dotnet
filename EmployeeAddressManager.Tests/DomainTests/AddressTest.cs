46f1e9af2085dc0e03954e95f7d4379c62a1c5e4
Cleberton <clebertonfgc@gmail.com>
2025-01-09 14:18:34 -0300

    unit tests to domain

---
+++ EmployeeAddressManager.Tests/DomainTests/AddressTest.cs
@@ -0,0 +1,40 @@
+﻿using EmployeeAddressManager.Domain.Entities;
+using EmployeeAddressManager.Domain.Validation;
+using FluentAssertions;
+
+namespace EmployeeAddressManager.Tests.DomainTests;
+
+public class AddressTest
+{
+    [Fact]
+    public void CreateAdress_WhithValidParams_ShouldCreateAdress()
+    {
+        Action action = () => new Address(1, "Street 123", "City 123", "State 123");
+        action.Should().NotThrow<DomainExceptionValidation>();
+    }
+
+    [Fact]
+    public void CreateAdress_WithInvalidStreet_NotShouldCreateAdress()
+    {
+        var street = string.Empty;
+        Action action = () => new Address(1, street, "City 123", "State 123");
+        action.Should().Throw<DomainExceptionValidation>().WithMessage($"{nameof(street)} cannot be null or empty.");
+    }
+    
+    [Fact]
+    public void CreateAdress_WithInvalidCity_NotShouldCreateAdress()
+    {
+        var city = string.Empty;
+        Action action = () => new Address(1, "Street 123", city, "State 123");
+        action.Should().Throw<DomainExceptionValidation>().WithMessage($"{nameof(city)} cannot be null or empty.");
+
+    }
+    
+    [Fact]
+    public void CreateAdress_WithInvalidState_NotShouldCreateAdress()
+    {
+        var state = string.Empty;
+        Action action = () => new Address(1, "Street 123", "City 123", state);
+        action.Should().Throw<DomainExceptionValidation>().WithMessage($"{nameof(state)} cannot be null or empty.");
+    }
+}
---
+++ EmployeeAddressManager.Tests/DomainTests/EmployeeTest.cs
@@ -0,0 +1,39 @@
+﻿using EmployeeAddressManager.Domain.Entities;
+using EmployeeAddressManager.Domain.Validation;
+using FluentAssertions;
+
+namespace EmployeeAddressManager.Tests.DomainTests;
+
+public class EmployeeTest
+{
+    [Fact]
+    public void CreateEmployee_WhithValidParams_ShouldCreateEmployee()
+    {
+        Action action = () => new Employee(1, "John Doe", "developer", new DateTime(2020, 01, 01));
+        action.Should().NotThrow<DomainExceptionValidation>();
+    }
+
+    [Fact]
+    public void CreateEmployee_WithInvalidName_NotShouldCreateEmployee()
+    {
+        var name = string.Empty;
+        Action action = () => new Employee(1, name, "developer", DateTime.Now);
+        action.Should().Throw<DomainExceptionValidation>().WithMessage($"{nameof(name)} cannot be null or empty.");
+    }
+    
+    [Fact]
+    public void CreateEmployee_WithInvalidRole_NotShouldCreateEmployee()
+    {
+        var role = string.Empty;
+        Action action = () => new Employee(1, "John Doe", role, DateTime.Now);
+        action.Should().Throw<DomainExceptionValidation>().WithMessage($"{nameof(role)} cannot be null or empty.");
+    }
+    
+    [Fact]
+    public void CreateEmployee_WithInvalidDate_NotShouldCreateEmployee()
+    {
+        var dateOfHiring = default(DateTime);
+        Action action = () => new Employee(1, "John Doe", "developer", dateOfHiring);
+        action.Should().Throw<DomainExceptionValidation>().WithMessage($"{nameof(dateOfHiring)} cannot be empty.");
+    }
+}
